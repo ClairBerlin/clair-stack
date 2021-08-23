ab08202617566f8c5c48087d6b191cba2990b842
Ulrich Schuster <ulrich.schuster@koing.de>
2021-08-23 11:53:40 +0200

    local MQTT broker
    Compose file that includes a local mosquitto broker, plus architecture diagram

---
+++ tools/compose-up.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ROOT_DIR=`dirname $0`
+. $ROOT_DIR/common.sh
+
+deploy_composition () {
+  docker login
+  docker-compose up -d -c $COMPOSE_FILES_DIR/docker-compose.yml $DOCKER_STACK_DEPLOY_ARGS --with-registry-auth $CLAIR_STACK_NAME
+  while true; do
+    echo "waiting for services to start..."
+    docker image ls --filter name=$CLAIR_STACK_NAME --format {{.Replicas}} | grep -q 0 || break
+    sleep 1
+  done
+  echo "done"
+}
+
+source_env_or_fail $1
+deploy_composition
