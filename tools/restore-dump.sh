799b5253d4efd7e83eb71cc16df8276ae4801f99
Jan Weil <jan.weil@decarbon1ze.com>
2021-12-09 19:20:32 +0100

    Fix stack prefix
    and add scale_db_services

--- tools/restore-dump.sh
+++ tools/restore-dump.sh
@@ -7,6 +7,11 @@ usage() {
   echo "usage: $SCRIPT_NAME env_file dump_file"
 }
 
+scale_db_services() {
+  replicas=$1
+  docker service scale ${CLAIR_STACK_NAME}_managair_server=$replicas ${CLAIR_STACK_NAME}_ingestair=$replicas
+}
+
 dump_db () {
   dump_file=$1
   test -f "$dump_file" || fail_usage "no dump file specified"
@@ -15,11 +20,11 @@ dump_db () {
   test -n "$SQL_PASSWORD" || fail "SQL_PASSWORD not set in environment file"
   DB_CONTAINER_ID=`eval_in_docker_context docker ps -qf "name=${CLAIR_STACK_NAME}_db"`
   test -n "$DB_CONTAINER_ID" || fail "db container not found"
-  docker service scale clair_managair_server=0 clair_ingestair=0
+  scale_db_services 0
   docker exec -e PGPASSWORD=$SQL_PASSWORD $DB_CONTAINER_ID psql -U $SQL_USER -d postgres -c "DROP DATABASE $SQL_DATABASE;" 
   docker exec -e PGPASSWORD=$SQL_PASSWORD $DB_CONTAINER_ID psql -U $SQL_USER -d postgres -c "CREATE DATABASE $SQL_DATABASE;" 
   docker exec -i $DB_CONTAINER_ID /bin/bash -c "PGPASSWORD=$SQL_PASSWORD psql -U $SQL_USER $SQL_DATABASE" < $dump_file
-  docker service scale clair_managair_server=1 clair_ingestair=1
+  scale_db_services 1
 }
 
 source_env_or_fail $1
